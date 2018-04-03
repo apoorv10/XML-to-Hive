d6f247d436ea6ac5a634dc40ea6f3e1a740fa22e
Apoorv Agrawal <apoorv10.agrawal@gmail.com>
2018-04-03 10:56:38 +0530

    Create flatten.scala

---
+++ flatten.scala
@@ -0,0 +1,69 @@
+package xml
+ 
+import org.apache.spark.SparkContext
+import org.apache.spark.SparkConf
+import org.apache.spark.sql.hive._
+import java.util.Properties
+import java.io.FileInputStream
+import java.io._
+import org.apache.spark.sql._
+import org.apache.spark.sql.types.{StructType,DataType,StructField, StringType, DoubleType,ArrayType,IntegerType, DateType, TimestampType};
+import org.apache.spark.sql.functions.{array, col, explode, lit, coalesce}
+
+ 
+object flatten {
+ 
+  def main(args: Array[String]) {
+ 
+    if (args.length < 1) {
+      System.err.println("Usage: XMLParser.jar <config.properties>")
+      println("Please provide the Configuration File for the XML Parser Job")
+      System.exit(1)
+    }
+ 
+    val sc = new SparkContext(new SparkConf().setAppName("Spark XML Process"))
+    val sqlContext = new HiveContext(sc)    
+    val dfSchema = sqlContext.read.format("com.databricks.spark.xml").option("rowTag","AttributionPopulationGroup").load(args(0))
+    
+    val flattened_DataFrame=flattenDf(dfSchema)
+  
+  }
+  
+    
+   
+  def flattenDf(df: DataFrame): DataFrame = {
+  var end = false 
+  var i = 0
+  val fields = df.schema.fields  
+  val fieldNames = fields.map(f => f.name) 
+  val fieldsNumber = fields.length 
+ 
+  while (!end) {
+    val field = fields(i) 
+    val fieldName = field.name 
+   
+ 
+    field.dataType match {  
+      case st: StructType =>
+         val childFieldNames = st.fieldNames.map(n => fieldName + "." + n +" as "+ fieldName+"_"+n)
+         val newFieldNames = fieldNames.filter(_ != fieldName) ++ childFieldNames
+         newFieldNames.foreach(println)
+         val newDf = df.selectExpr(newFieldNames: _*)
+         return flattenDf(newDf)
+       
+      case at: ArrayType =>
+        val fieldExplode = df.select(explode(col(s"$fieldName")).alias(s"$fieldName"))
+        val fieldExplode1 = fieldExplode.selectExpr(s"$fieldName.*")
+        val structFields = fieldExplode1.schema.simpleString
+        val explodedDf = df.selectExpr((fieldNames: _*)).withColumn(s"$fieldName", explode(coalesce(col(s"$fieldName"),array(lit(null).cast(structFields)))))
+        return flattenDf(explodedDf)
+      case _ => Unit
+    }
+ 
+    i += 1
+    end = i >= fieldsNumber
+  }
+  df
+}
+ 
+} 
